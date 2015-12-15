2373e4c8300398f039784815c775c1f6ce741daf
Christian Croft <christian@usell.com>
2015-12-15 13:29:12 -0500

    Open auctions brought into master Auctions axure project

---
+++ files/sealed_auction/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m,n,o,p,q,r,s,t,u],v,_(w,x,y,z,A,B,C,_(),D,_(E,F,G,H,I,_(J,K,L,M),N,null,O,H,P,H,Q,R,S,null,T,U,V,W,X,Y,Z,U),ba,_(),bb,_(),bc,_(bd,[])),be,_(),bf,_());}; 
+var b="url",c="sealed_auction.html",d="generationDate",e=new Date(1450203991779.02),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="email",k="signup",l="pw",m="CustomerCareLoggedIn",n="Filter_CarrierSet",o="Filter_ManufacturerSet",p="PaymentDetails_ShowAck",q="LotAddedFlag",r="LotDeletedFlag",s="LotImageWhich",t="LotImageCount",u="AuctionDeletedFlag",v="page",w="packageId",x="ea0a3aeec1c24d50a9d0e0a8ab4d70b0",y="type",z="Axure:Page",A="name",B="Sealed Auction",C="notes",D="style",E="baseStyle",F="627587b6038d43cca051c114ac41ad32",G="pageAlignment",H="near",I="fill",J="fillType",K="solid",L="color",M=0xFFFFFFFF,N="image",O="imageHorizontalAlignment",P="imageVerticalAlignment",Q="imageRepeat",R="auto",S="favicon",T="sketchFactor",U="0",V="colorStyle",W="appliedColor",X="fontName",Y="Applied Font",Z="borderWidth",ba="adaptiveStyles",bb="interactionMap",bc="diagram",bd="objects",be="masters",bf="objectPaths";
+return _creator();
+})());
